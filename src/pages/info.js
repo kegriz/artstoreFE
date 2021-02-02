e04c06765579c6bcc2a5463715cf343d5b1a3735
Rafał Rudko <rafal.rudko@rb.com>
2021-02-02 23:30:34 +0100

    add new routing - info page

---
+++ src/pages/info.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const InfoPage = () => {
+  return (
+    <main>
+      <title>Info Page</title>
+
+      <h1 className="title title--red">Info Page</h1>
+
+      <p>info</p>
+    </main>
+  );
+};
+
+export default InfoPage;
